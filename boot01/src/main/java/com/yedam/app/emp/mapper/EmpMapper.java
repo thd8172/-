62e91706fd2cdf225b0d7f4edf15d45fa5758b45
ssh <thd8172@naver.com>
2024-07-10 17:46:21 +0900

    .

---
+++ boot01/src/main/java/com/yedam/app/emp/mapper/EmpMapper.java
@@ -0,0 +1,24 @@
+package com.yedam.app.emp.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.yedam.app.emp.service.EmpVO;
+
+public interface EmpMapper {
+	// 테이블을 기준
+	// 전체조회
+	public List<EmpVO> selectEmpAll();
+	// 단건조회
+	public EmpVO selectEmpInfo(EmpVO empVO);
+	// 등록
+	public int insertEmpInfo(EmpVO empVO);
+	// 수정
+	public int updateEmpInfo(@Param("id")int empId,
+							 @Param("emp")EmpVO empVO);
+	// 삭제
+	public int deleteEmpInfo(int empId);
+	
+	
+}
---
+++ boot01/src/test/java/com/yedam/app/Boot01ApplicationTests.java
@@ -0,0 +1,81 @@
+package com.yedam.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpVO;
+
+@SpringBootTest
+class Boot01ApplicationTests {
+	
+	@Autowired
+	EmpMapper empMapper;
+	
+//	@Test
+//	void contextLoads() {
+//		assertNotNull(empMapper);
+//	}
+//	
+//	@Test
+//	void selectEmpAll() {
+//		//전체조회
+//		List<EmpVO> list = empMapper.selectEmpAll();
+//		assertTrue(!list.isEmpty());
+//	}
+//	
+//	@Test
+//	void selectEmpInfo() {
+//		//단건조회
+//		EmpVO empVO = new EmpVO();
+//		empVO.setEmpid(114);
+//		EmpVO findVO = empMapper.selectEmpInfo(empVO);
+//		assertEquals(findVO.getEmpname(), "Raphaely");
+//	}
+//	
+	@Test
+	void insertEmpInfo() {
+		//등록
+		EmpVO empVO = new EmpVO();
+		empVO.setEmpname("Hong");
+		empVO.setSal(1000);
+		empVO.setDeptid(40);
+		
+		int result = empMapper.insertEmpInfo(empVO);
+		assertEquals(result, 1);
+		assertEquals(empVO.getEmpid(), 208);
+
+		
+	}
+//	@Test
+//	void updateEmpInfo() {
+//		//1.단건조회 => 2.업데이트 
+//		//1-1) 조건 뭘조회할지 를 empVO에
+//		EmpVO empVO = new EmpVO();
+//		empVO.setEmpid(100);
+//		
+//		//1-2) 조회 (분리해서 호출하는 메소드에 요청)
+//		EmpVO findVO = empMapper.selectEmpInfo(empVO);
+//		findVO.setEmpname("Kang");
+//		findVO.setMgr(114);
+//		
+//		//2. 업데이트 
+//		int result 
+//		= empMapper.updateEmpInfo(findVO.getEmpid(), findVO);
+//	}
+	
+	//삭제 
+//	@Test
+//	void deleteEmpInfo() {
+//		int result = empMapper.deleteEmpInfo(102);
+//		assertEquals(1, result);
+//	}
+
+}
